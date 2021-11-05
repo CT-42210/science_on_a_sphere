2ecac1c203764a0a78bce915780084110db2140a
Nick T <56010135+CT-42210@users.noreply.github.com>
2021-11-05 11:02:21 -0400

    Update main.py
    added usb talk thing

--- main.py
+++ main.py
@@ -1,26 +1,52 @@
 import PySimpleGUI as sg
+import serial
 
 sg.theme('Dark Grey 13')
 
+ser = serial.Serial('/dev/tty.usbmodem102',9600)
+
 def button1():
-    print('Button 1')
+    encoded = str.encode("15,15\n")
+    ser.write(encoded)
 
 def button2():
-    print('Button 2')
+    encoded = str.encode("0,15\n")
+    ser.write(encoded)
 
 def button3():
-    print("button 3")
+    encoded = str.encode("15,0\n")
+    ser.write(encoded)
+
+def button4():
+    encoded = str.encode("0,0\n")
+    ser.write(encoded)
 
 dispatch_dictionary = {
     '1':button1,
     '2':button2,
-    '3':button3
+    '3':button3,
+    '4':button4
 }
 
 layout = [[sg.Text('Click a button', auto_size_text=True)],
-          [sg.Button('1'), sg.Button('2'), sg.Button('3'), sg.Quit()]]
+          [sg.Button('1'), sg.Button('2'), sg.Button('3'), sg.Button('4'), sg.Quit()]]
 
 window = sg.Window('>:)', layout)
+
+def byte_machine(string):
+
+    # string with encoding 'utf-8'
+    arr = bytes(string, 'utf-8')
+    arr2 = bytes(string, 'ascii')
+
+    print(arr, '\n')
+
+    # actual bytes in the the string
+    for byte in arr:
+        print(byte, end=' ')
+    print("\n")
+    for byte in arr2:
+        print(byte, end=' ')
 
 while True:
     event, value = window.read()
